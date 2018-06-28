d559dba43df78fe4dd2255d2c730ffc93406c4c5
Marcel Badua <bitterdash@gmail.com>
2018-06-28 11:16:59 +0800

    'initial'

---
+++ spec/atom-cerberus-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCerberusSnippetsView from '../lib/atom-cerberus-snippets-view';
+
+describe('AtomCerberusSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
